7c530971b41ceae1f861db907c041df5bec97607
David Kelly <kellydavid178@gmail.com>
2014-10-29 00:20:21 +0000

    Initial Commit

---
+++ SerialDataGrab/DataConnection/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace SerialDataGrab.DataConnection
+{
+    class Connection
+    {
+        // Constants
+        private const int RECEIVED_BYTES_THRESHOLD = 1;
+
+        private SerialPort serialPort = null;
+    }
+}
